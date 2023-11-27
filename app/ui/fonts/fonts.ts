1c53853f12021951460a825c43f4d4169bba7ecb
Fredd <freddfr@Fredds-MacBook-Pro.local>
2023-11-27 12:09:46 -0400

    [fredd][27 11 2023 12 10] inicio con nextjs14

---
+++ app/ui/fonts/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lato, Lusitana, Modak } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const modak = Modak({ subsets: ['latin'], weight: ['400'] });
+export const lato = Lato({ subsets: ['latin'], weight: ['400'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400'] });
